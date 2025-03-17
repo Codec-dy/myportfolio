d66bf429afa542dea6236697b07fb67d5c73bd02
Codec-dy <yormesordaniel@gmail.com>
2025-03-17 00:02:56 -0500

    just Updated

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter as Router } from 'react-router-dom'
-import PortfolioContextProvider from './context/PortfolioContext.jsx'
+import { BrowserRouter,HashRouter as Router } from 'react-router-dom'
+import PortfolioContextProvider from './context/portfolioContext.jsx'
 
 
 createRoot(document.getElementById('root')).render(
